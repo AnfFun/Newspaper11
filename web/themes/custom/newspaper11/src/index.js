6028db32e781de7e02d52ad9ec1cc97daae8f706
AnfFun <m.prokofiev@dev-branch.com>
2022-05-20 13:30:07 +0300

    (main) Fixing some troble with index.js

--- web/themes/custom/newspaper11/src/index.js
+++ web/themes/custom/newspaper11/src/index.js
@@ -1,6 +1,5 @@
-import './scss/Global.scss'
-import './scss/front_views.scss'
 import './scss/Global.scss';
-import './scss/Header.scss'
+import './scss/Header.scss';
+import './scss/front_views.scss';
 import './scss/Footer.scss';
 
